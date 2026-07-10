625cbb07e612e3d38ac93eb07e4df6add868a7f6
dylan18x <gadieldylan02@gmail.com>
2026-07-10 08:52:38 -0500

    SHOPAPP CHECKPOINT 03 - AUTH  - 10-07-2026

--- modulo_4/reactjs/shopapp-react/src/infrastructure/config/api.config.ts
+++ modulo_4/reactjs/shopapp-react/src/infrastructure/config/api.config.ts
@@ -1,6 +1,6 @@
 // src/infrastructure/config/api.config.ts
 
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000/api',
+  BASE_URL: import.meta.env.VITE_API_BASE_URL ?? 'https://higuera-shopapi.uaeftt-ute.site/api',
   TIMEOUT: 10_000,
 } as const
